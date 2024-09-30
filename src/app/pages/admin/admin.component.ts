eba1dc29c8d8ec4ea1c44d860e3d3c3403f88310
leandro <leaguts21@gmail.com>
2024-09-29 23:43:59 -0500

    productos en admin

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-admin',
@@ -12,10 +13,38 @@ export class AdminComponent implements OnInit {
   cargo: string | null = null;
   name: string | null = null;
 
-  constructor() { }
+  productsData:Array<any>=[];
+
+  constructor( private  ProductService : ProductsService  ) { }
 
   ngOnInit(): void {
     this.cargo = sessionStorage.getItem('rol');
     this.name = sessionStorage.getItem('username');  
+
+    this.ProductService.getAllProducts().subscribe({
+      next:(res)=>{
+        this.productsData = res;
+        console.log(res);
+        
+      },
+      error:(err)=>{
+        console.log(err);
+        
+      }
+    })
+
+    throw new Error('Method not implemented.');
   }
-}
+
+
+
+  }
+
+
+
+
+
+
+
+
+

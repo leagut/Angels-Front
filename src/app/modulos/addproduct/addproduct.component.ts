43c17f321e4e5a725edfc454784b90b4aa42e4eb
leandro <leaguts21@gmail.com>
2024-11-13 23:28:07 -0500

    crud del producto completo

--- src/app/modulos/addproduct/addproduct.component.ts
+++ src/app/modulos/addproduct/addproduct.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
+import { ProductsService } from '../../services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-addproduct',
@@ -21,7 +23,8 @@ export class AddproductComponent {
 
   constructor(
     private fb: FormBuilder,
-    private dialogRef: MatDialogRef<AddproductComponent>
+    private dialogRef: MatDialogRef<AddproductComponent>,
+    private  productService : ProductsService 
   ) {
     this.productForm = this.fb.group({
       name: ['', Validators.required],
@@ -36,10 +39,44 @@ export class AddproductComponent {
     this.dialogRef.close();
   }
 
-  onSave(): void {
-    if (this.productForm.valid) {
-      this.dialogRef.close(this.productForm.value);
-    }
+  private getCategoriaId(categoria: string): number {
+    const categorias: Record<string, number> = {
+        'belleza': 4,
+        'alimenticio': 5,
+        'aseo-personal': 6,
+        'limpieza': 7
+    };
+    return categorias[categoria] || 0; // Retorna 0 o algún valor por defecto si no coincide
+}
+
+
+onSave(): void {
+  if (this.productForm.valid) {
+      // Clonamos el objeto para evitar modificar directamente el formulario
+      const productData = { ...this.productForm.value };
+
+      // Convertimos el nombre de la categoría en su ID numérico y lo asignamos a categoryId
+      productData.categoryId = this.getCategoriaId(productData.categoryId);
+
+      console.log("Datos a enviar:", productData); // Confirmamos que los datos están en el formato correcto
+
+      // Llamada al servicio para guardar el producto
+      this.productService.addProduct(productData).subscribe(
+          response => {
+              console.log("Producto guardado con éxito:", response);
+              this.dialogRef.close(productData);
+          },
+          error => console.error("Error al guardar el producto:", error)
+      );
   }
+}
+
+
+
+
+ 
+
+
+
 
 }

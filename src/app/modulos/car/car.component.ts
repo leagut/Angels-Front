1c82a5cb85eb4520732cac7dcc715ecc9f4f6c68
leandro <leaguts21@gmail.com>
2024-12-03 01:01:59 -0500

    envio de formulario compra

--- src/app/modulos/car/car.component.ts
+++ src/app/modulos/car/car.component.ts
@@ -12,6 +12,8 @@ export class CarComponent implements OnInit, OnDestroy {
   @Input() showModal: boolean = false;
   cartItems: any[] = [];
   totalSuma: number = 0;
+  direccion: string = ''; // Propiedad para la dirección
+  telefono: string = '';  // Propiedad para el teléfono
 
   
 
@@ -49,4 +51,50 @@ export class CarComponent implements OnInit, OnDestroy {
   actualizarSuma(): void {
     this.totalSuma = this.cartItems.reduce((total, item) => total + item.price, 0);
   }
+
+  generarNumeroFactura(): string {
+    const fecha = new Date();
+    const año = fecha.getFullYear();
+    const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+    const dia = String(fecha.getDate()).padStart(2, '0');
+    const hora = String(fecha.getHours()).padStart(2, '0');
+    const minutos = String(fecha.getMinutes()).padStart(2, '0');
+  
+    // Generar una cadena alfanumérica aleatoria de 5 caracteres
+    const randomString = Math.random().toString(36).substring(2, 7).toUpperCase();
+  
+    // Combinación de fecha, hora y cadena aleatoria
+    return `F${año}${mes}${dia}-${hora}${minutos}-${randomString}`;
+  }
+
+  realizarCompra(): void {
+    const numeroFactura = this.generarNumeroFactura();
+
+    const compra = {
+      numeroFactura,
+      direccion: this.direccion,
+      telefono: this.telefono,
+      total: this.totalSuma,
+      preventas: this.cartItems.map(item => ({
+        productId: item.productId,
+        name: item.name,
+        price: item.price
+      }))
+    };
+
+    console.log('JSON para el backend:', compra);
+
+  }
+
+  camposValidos(): boolean {
+    return this.direccion.trim().length > 0 && this.telefono.trim().length > 0;
+  }
+
+  permitirSoloNumeros(event: KeyboardEvent): void {
+    const charCode = event.key.charCodeAt(0);
+    if (charCode < 48 || charCode > 57) {
+      event.preventDefault();
+    }
+  }
+
 }

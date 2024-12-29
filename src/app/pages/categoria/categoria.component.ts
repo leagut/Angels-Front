b5b9eb1f6f708f03aa41dd2cbcd21d721233e1d3
leandro <leaguts21@gmail.com>
2024-12-28 22:04:42 -0500

    funcionando la confirmacion de ventas

--- src/app/pages/categoria/categoria.component.ts
+++ src/app/pages/categoria/categoria.component.ts
@@ -12,6 +12,7 @@ export class CategoriaComponent {
   pedido: any = null; // Almacena los datos del pedido
   loading: boolean = false; // Indica si la petición está cargando
   error: string | null = null; // Mensaje de error
+  confirmacionVenta: boolean = false;
   @ViewChild('pedidoForm') pedidoForm?: NgForm;
 
   constructor(private pedidoService: PedidoServiceService) {}
@@ -39,12 +40,59 @@ export class CategoriaComponent {
     
   }
 
-  imprimirPedido(): void {
 
-    console.log("pedi",this.pedido);
-    
+
+
+  imprimirPedido(): void {
+    if (this.confirmacionVenta) {
+      // Verifica si `pedido` tiene contenido válido
+      if (!this.pedido) {
+        this.error = 'No se ha cargado ningún pedido para confirmar la venta.';
+        return;
+      }
   
+      console.log('Confirmando la venta...');
+  
+      this.pedidoService.confirmarCompraEfectivo(this.pedido).subscribe({
+        next: (response) => {
+          console.log('Venta confirmada:', response);
+          alert('Venta confirmada exitosamente');
+          this.pedido = null; // Limpia el pedido tras confirmarlo
+        },
+        error: (err) => {
+          console.error('Error al confirmar la venta:', err);
+  
+          if (err.status === 400) {
+            this.error = 'La factura ya ha sido gestionada previamente.';
+            this.pedido = null; 
+            
+          } else if (err.status === 500 && err.error?.error?.includes('UK')) {
+            this.error = 'La factura ya ha sido gestionada previamente.';
+            this.pedido = null; 
+            
+          } else {
+            this.error = 'Ocurrió un error al confirmar la venta. Por favor, intente nuevamente.';
+            alert(this.error);
+          }
+        },
+      });
+    } else {
+      this.error = 'Debe confirmar la venta marcando la casilla correspondiente.';
+      alert(this.error);
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

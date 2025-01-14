28da5c502a9cdba3baf9fd7b483818674f972d61
leandro <leaguts21@gmail.com>
2025-01-14 14:04:59 -0500

    api gateway5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  urlHost:'https://lnbggvcmnf.execute-api.us-west-2.amazonaws.com/prod/',   
+  urlHost:'https://lnbggvcmnf.execute-api.us-west-2.amazonaws.com/',   
   userId:1
 };
 //https://lnbggvcmnf.execute-api.us-west-2.amazonaws.com/prod/

6c928acf0d9db5bb8943251dd5509ca1e4402296
leandro <leaguts21@gmail.com>
2025-01-15 12:06:11 -0500

    intento2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  urlHost:'http://54.212.15.91:8080/',   
-  userId:1
+  urlHost: 'https://angels-front.vercel.app/', // Apunta a Vercel, que redirige al backend
+  userId: 1
 };
 //https://lnbggvcmnf.execute-api.us-west-2.amazonaws.com/prod/
 //urlHost:'http://54.212.15.91:8080/', 
